67bd207dfb516f2c6687c3c1dc1774dfbfe1e7e4
Andrew <awoodcoc@indiana.edu>
2023-08-10 16:39:13 -0500

    exercises work

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,16 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+  let pageTitle = "My Book List";
+  let book1 =
+    "https://upload.wikimedia.org/wikipedia/commons/c/c3/Remarque_Im_Westen_nichts_Neues_1929.jpg";
+  let book2 = "";
+  let book3 = "";
 
-   return (
-      <div>
-         <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
-      </div>      
-   );
+  return (
+    <div>
+      <h3>{pageTitle}</h3>
+      <img src={book1} alt="Nothing here!" />
+      <img src={book2} alt="Nothing here!" />
+      <img src={book3} alt="Nothing here!" />
+    </div>
+  );
 }
--- part1/exercises/src/components/ChoresList.js
+++ part1/exercises/src/components/ChoresList.js
@@ -1,3 +1,5 @@
-export default function ChoresList () {
-   return <p>No content is here!</p>;
+import classes from "./ChoresList.module.css";
+
+export default function ChoresList() {
+  return <p className={classes.choresText}>No content is here!</p>;
 }
